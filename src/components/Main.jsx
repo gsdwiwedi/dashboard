e8c82307a4e825ba99b658376e81d4538af76b75
gsdwiwedi <88853051+gsdwiwedi@users.noreply.github.com>
2025-02-12 12:01:57 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,14 @@ const Main = () => {
   return (
     <div>
         <Outlet></Outlet>
-
+         <a
+        href="https://chatbot-one-sand-26.vercel.app"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="chatbot-link"
+      >
+        <FaRobot /> Chat with us!
+      </a>
     </div>
   )
 }
