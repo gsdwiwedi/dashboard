30aba3cfb58417323e90b18679acf32f4021e341
gsdwiwedi <88853051+gsdwiwedi@users.noreply.github.com>
2025-02-12 11:57:41 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ const Main = () => {
   return (
     <div>
         <Outlet></Outlet>
-      <a>https://chatbot-one-sand-26.vercel.app/</a>
+      
        <a href="https://chatbot-one-sand-26.vercel.app/" rel="noopener noreferrer">
         Go to Example App
       </a>
