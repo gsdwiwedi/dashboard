c2175f70da7b94b11461e1f89e06c155b3d57c21
gsdwiwedi <88853051+gsdwiwedi@users.noreply.github.com>
2025-02-12 12:15:28 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet} from 'react-router'
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 
 const Main = () => {
    useEffect(() => {
