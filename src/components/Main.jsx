c0db1b76f2a9131705d46b9194174751d326314d
gsdwiwedi <88853051+gsdwiwedi@users.noreply.github.com>
2025-02-12 14:22:14 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,18 +3,7 @@ import { Outlet} from 'react-router'
 import { useEffect } from 'react';
 
 const Main = () => {
-   useEffect(() => {
-    // Load the chatbot script dynamically
-    const script = document.createElement('script');
-    script.src = 'chatbot-one-sand-26.vercel.app';
-    script.async = true;
-    document.body.appendChild(script);
-
-    // Cleanup the script on component unmount
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+ 
   
   return (
     <div>
